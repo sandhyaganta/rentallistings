a585429ff3c92d4ad71346819d569a0519e8e4b8
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-07-29 11:25:09 +0530

    home component completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:HomepageComponent},
-    {path:'dash',component:DashboardComponent}
+    {path:'dash',component:DashboardComponent,children:[
+        {path:'home1',component:HomeComponent}
+    ] }
 ];
